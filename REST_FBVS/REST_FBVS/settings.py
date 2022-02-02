2c9b261cc42463af92ca3194812a6fea0bce8e8a
shivshankar31 <shivshankar31@yahoo.com>
2022-02-02 05:50:14 +0530

    27: step 1: create project app, database setup

--- REST_FBVS/REST_FBVS/settings.py
+++ REST_FBVS/REST_FBVS/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fbvapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'REST_FBVS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'StudentDB',
+        'USER' : 'root',
+        'PASSWORD': ''
     }
 }
 
