7229ca660d95f7a85615302ab2c3a6c9ba85d018
shivshankar31 <shivshankar31@yahoo.com>
2022-02-02 06:09:36 +0530

    28: step 2: create database, models

--- REST_FBVS/fbvapp/models.py
+++ REST_FBVS/fbvapp/models.py
@@ -1,3 +1,14 @@
+from tokenize import Name
+from unicodedata import decimal
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+    Id = models.IntegerField(primary_key=True, null=False)
+    Name = models.CharField(max_length=50, null=False)
+    Mark = models.DecimalField( max_digits=3, decimal_places=2)
+    Location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.Id + self.Name + self.Mark + self.location
