0078dcab19615f96a2a6dfd09b92c819a9fd923d
shivshankar31 <shivshankar31@yahoo.com>
2022-02-05 04:44:08 +0530

    32: step 5: create detail view function

--- REST_FBVS/fbvapp/views.py
+++ REST_FBVS/fbvapp/views.py
@@ -17,3 +17,26 @@ def student_list(request):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def student_detail(request,pk):
+    try:
+        stud = Students.objects.get(pk=pk)
+    except Students.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(stud)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(stud,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'delete':
+        stud.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
