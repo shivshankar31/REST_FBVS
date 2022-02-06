01acae0e488ce437d5eabd7d7f78d2518cb9d9f7
shivshankar31 <shivshankar31@yahoo.com>
2022-02-06 07:19:11 +0530

    40: step 9: create class for get,put,delete

--- REST_FBVS/cbvapp/views.py
+++ REST_FBVS/cbvapp/views.py
@@ -4,9 +4,10 @@ from cbvapp.models import Employees
 from cbvapp.serializers import EmployeeSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404, response
 # Create your views here.
 
-class Employeelist(views.APIView):
+class EmployeeList(views.APIView):
 
     def get (self,request):
         emp = Employees.object.all()
@@ -21,6 +22,31 @@ class Employeelist(views.APIView):
         return Response(empseri.error, status=status.HTTP_400_BAD_REQUEST)
 
         
+class EmployeesDetails(views.APIView):
+
+    def get_object(self,pk):
+        try:
+            return Employees.object.get(pk=pk)
+        except Employees.DoesNotExist:
+            raise Http404
+    
+    def get(self,pk): # request deleted
+        emp = self.get_object(pk)
+        empdetail = EmployeeSerializer(emp.data) # as not in the learing
+        return Response(empdetail)
+
+    def put(self, request, pk):
+        emp = self.get_object(pk)
+        empupdate = EmployeeSerializer(emp, data=request.data)
+        if empupdate.is_valid():
+            empupdate.save()
+            return Response(empupdate.data, status = status.HTTP_200_OK)
+        return Response(empupdate.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        emp = self.get_object(pk)
+        emp.delete()
+        return Response("Delete Successfull", status = status.HTTP_204_NO_CONTENT)
 
 
 
